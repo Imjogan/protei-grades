9fb9dfe62fa9c993a5d34ba4daeca4254e2b7896
minin-i <minin-i@protei-lab.ru>
2025-08-06 16:12:18 +0300

    add middle::dev+tools::nginx

--- middle/dev+tools/shell-script.sh
+++ middle/dev+tools/shell-script.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Поиск всех TODO во всех JS/TS-файлах проекта
 grep -rnw --include="*.ts" --include="*.js" "TODO" .
-# Этот скрипт ищет все TODO во всех JS/TS-файлах проекта
 
+# Быстрая очистка кэшей и папок сборки
+echo "Удаляем папки cache и build..."
+
+rm -rf node_modules/.cache
+rm -rf dist
+rm -rf build
+
+echo "Очистка завершена!"
